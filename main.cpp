f8d971f6f0f51929f6c38ed68fc7e9de71465174
MooreKyle <moorekl@protonmail.com>
2024-03-04 23:02:30 -0500

    Added project info. Added commments to enhance code clarity

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+// Kyle Moore
+// March 4, 2024
+// CIS 4634-003 - Applied Cryptography & Security
+// Data Encryption Standard DES
+
+// Libraries
 #include <iostream>   // include iostream library to use cout (output) and cin (input) functions
 #include <string>     // include string library to use string data type
 
@@ -15,23 +21,24 @@ string decrypt(const string& ciphertext, const string& key) {
     return "decrypted_text";
 }
 
+// main function
 int main() {
     string plaintext, key;     // declarations to store plaintext & key
     
     // Get plaintext from the user
     cout << "Enter the plaintext: ";
-    getline(cin, plaintext);
+    getline(cin, plaintext);     //NOTE: getline function is used as it ensures the entire line, including spaces (unlike cin, which stops reading at the first whitespace), is read into 'plaintext' and 'key' variables.
     
     // Get the secret key from the user
     cout << "Enter the secret key: ";
     getline(cin, key);
     
     // Encrypt the plaintext
-    string ciphertext = encrypt(plaintext, key);
+    string ciphertext = encrypt(plaintext, key);     //
     cout << "Ciphertext: " << ciphertext << endl;
     
     // Decrypt the ciphertext
-    string decryptedText = decrypt(ciphertext, key);
+    string decryptedText = decrypt(ciphertext, key);     // attempt to recover the original plaintext & show result of decryption process
     cout << "Decrypted text: " << decryptedText << endl;
 
     return 0;     // Successful termination of the program
